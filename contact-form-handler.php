9387216fa8fba74b2660742e31bd6b0860226a63
ParancibiaASF <parancibia@aconcaguasf.com>
2021-04-06 13:07:31 -0300

    Added functional form and changed colors

---
+++ contact-form-handler.php
@@ -0,0 +1,24 @@
+﻿<?php
+    $name = $_Post['name'];
+    $visitor_email = $_Post['email'];
+    $message = $_Post['message'];
+    
+    $email_from = 'hello@waffle.social'
+    
+    $email_subject = 'New Form Submission';
+    
+    $email_body =   "User name: $name.\n".
+                    "User email: $visitor_email.\n".
+                    "User Message: $message.\n";
+                    
+    $to = "hello@waffle.social"
+    
+    $headers = "From: $email_from \r\n"
+    
+    $headers .= "Reply-To: $visitor_email \r\n";
+    
+    mail($to,$email_subject,$email_body,$headers);
+    
+    header("Location: index.html");
+
+?>
---
+++ send.php
@@ -0,0 +1,13 @@
+﻿<?php
+	if(isset($_POST["nombre"]) && isset($_POST["email"]) && isset($_POST["telefono"]) && isset($_POST["mensaje"]) ){
+		$to = "hello@waffle.social";
+		$subject = "Consulta desde vitaminavida.com.ar";		
+		$contenido = "Nombre: ".$_POST["nombre"]."\n\nEmail: ".$_POST["email"]."\n\nTelefono: ".$_POST["telefono"]."\n\nMensaje: ".$_POST["mensaje"]."\n\n";
+		$header = "From: hello@waffle.social\nReply-To: ".$_POST["email"]."\n";
+		$header .= "Mime-Version: 1.0\n";
+		$header .= "Content-Type: text/plain";
+		if(mail($to, $subject, $contenido ,$header)){
+			include 'confirmation.html';
+		}
+	}
+?>
